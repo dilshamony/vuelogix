10355970c99ac543b2688531e925463ffbf31022
Dilsha <dilsham998@gmail.com>
2021-07-01 19:57:00 +0530

    commit

--- employee.py
+++ employee.py
@@ -1,22 +1,22 @@
 #converting datas to dictionary format
 employees = {
-    1001: {"id": 1000, "Name": "Vishal", "Gender": "Male", "Experience": 4 ,"Department":"HR"},
-    1002: {"id": 1001, "Name": "Rani", "Gender": "Female", "Experience": 1,"Department":"Technical Support"},
-    1003: {"id": 1002, "Name": "Divya", "Gender": "Female", "Experience": 2,"Department":"Junior Trainee"},
-    1004: {"id": 1003, "Name": "Jyothy", "Gender": "Female", "Experience": 3,"Department":"Technical Lead"},
-    1005: {"id": 1000, "Name": "Venu", "Gender": "Male", "Experience": 1,"Department":"Program coordinator"},
-    1006: {"id": 1001, "Name": "Manu", "Gender": "Male", "Experience": 6,"Department":"Technical Support"},
-    1007: {"id": 1002, "Name": "Jose", "Gender": "Male", "Experience": 3,"Department":"Technical Support"},
-    1008: {"id": 1003, "Name": "Munna", "Gender": "Male", "Experience": 3,"Department":"Program assistant"},
-    1009: {"id": 1003, "Name": "Rose", "Gender": "Female", "Experience": 2,"Department":"Software Tester"},
-    1010: {"id": 1003, "Name": "Sam", "Gender": "Male", "Experience": 5,"Department":"QA"}
+    1001: {"Employee Id": 1001, "Employee Name": "Vishal", "Gender": "Male", "Years of Experiance": 2 ,"Department":"HR"},
+    1002: {"Employee Id": 1002, "Employee Name": "Hima", "Gender": "Female", "Years of Experiance": 4,"Department":"Technical Support"},
+    1003: {"Employee Id": 1003, "Employee Name": "Fathima", "Gender": "Female", "Years of Experiance": 1,"Department":"Junior Trainee"},
+    1004: {"Employee Id": 1004, "Employee Name": "Jeeva", "Gender": "Male", "Years of Experiance": 4,"Department":"Technical Lead"},
+    1005: {"Employee Id": 1005, "Employee Name": "Gini", "Gender": "Female", "Years of Experiance": 3,"Department":"Program Coordinator"},
+    1006: {"Employee Id": 1006, "Employee Name": "Manu", "Gender": "Male", "Years of Experiance": 7,"Department":"Technical Lead"},
+    1007: {"Employee Id": 1007, "Employee Name": "Jennifier", "Gender": "Female", "Years of Experiance": 3,"Department":"Technical Support"},
+    1008: {"Employee Id": 1008, "Employee Name": "Mithun", "Gender": "Male", "Years of Experiance": 2,"Department":"Program Trainee"},
+    1009: {"Employee Id": 1009, "Employee Name": "Kavitha", "Gender": "Female", "Years of Experiance": 1,"Department":"Software Training"},
+    1010: {"Employee Id": 1010, "Employee Name": "Sakthi", "Gender": "Male", "Years of Experiance": 4,"Department":"Technical Support"}
 }
 
 #function for accepting id ,property and to print the result
 def getemployeedetails(**kwargs):
     id = kwargs["id"]
     if id in employees:
-        print("Employee Name : ",employees[id]["Name"])
+        print("Employee Name : ",employees[id]["Employee Name"])
         if prop in employees[id]:
             print(prop," : ",employees[id][prop])
         else:
